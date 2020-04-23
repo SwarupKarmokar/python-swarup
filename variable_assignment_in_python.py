12a90868eba2bbbd88860d62d3a441842fc11b8b
SwarupKarmokar <59216779+SwarupKarmokar@users.noreply.github.com>
2020-04-23 20:59:35 +0530

    variable assignment added

---
+++ variable_assignment_in_python.py
@@ -0,0 +1,43 @@
+# we can store values in variable
+variable1 = 2
+print(variable1)
+
+variable1, variable2 = 2, 4
+print(variable1)
+print(variable2)
+
+# we can store values into variable using int,float,list,tuple,dictionary,set,boolean.
+# use the type method for checking the type of the variable
+
+# using string
+name = 'Swarup'
+print(name)
+print(type(name))
+
+# using list
+my_dogs = ['Kalu', 'Lalu']
+print(my_dogs)
+print(type(my_dogs))
+
+# using tuple
+my_tuple = (1, 2, 3, 4, 5, 6)
+print(my_tuple)
+print(type(my_tuple))
+
+# using dictionary
+my_dict = {'value1': 2, 'value2': 'jhope'}
+print(my_dict)
+print(type(my_dict))
+
+# using set
+my_set = set()
+my_set.add(5)
+print(my_set)
+print(type(my_set))
+
+# using boolean
+my_bool = True  # or False
+print(my_bool)
+print(type(my_bool))
+
+
