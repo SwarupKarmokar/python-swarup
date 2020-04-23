563504c2ef6ac8b9e087599f52b2303b0bc10c34
SwarupKarmokar <59216779+SwarupKarmokar@users.noreply.github.com>
2020-04-23 15:16:10 +0530

    add math_problems

---
+++ basic_math.py
@@ -0,0 +1,59 @@
+# we can doing this for normal calculation
+# for addition
+print(2+3)
+
+# when user want to give input
+variable1 = int(input('enter the first value=> '))
+variable2 = int(input('enter the second value=> '))
+variable3 = variable1 + variable2
+print(variable3)
+
+# for subtraction
+print(2-1)
+
+# when user want to give input
+variable1 = int(input('enter the first value=> '))
+variable2 = int(input('enter the second value=> '))
+variable3 = variable1 - variable2
+print(variable3)
+
+# for multiplication
+print(2*4)
+# when user want to give input
+variable1 = int(input('enter the first value=> '))
+variable2 = int(input('enter the second value=> '))
+variable3 = variable1 * variable2
+print(variable3)
+
+# for division(its gives us float value)
+print(4/2)
+
+# when user want to give input
+variable1 = int(input('enter the first value=> '))
+variable2 = int(input('enter the second value=> '))
+variable3 = variable1 / variable2
+print(variable3)
+
+# for floor values
+print(5//2)
+
+# when user want to give input
+variable1 = int(input('enter the first value=> '))
+variable2 = int(input('enter the second value=> '))
+variable3 = variable1 // variable2
+print(variable3)
+
+# for modulo value
+print(9 % 5)
+
+# when user want to give input
+variable1 = int(input('enter the first value=> '))
+variable2 = int(input('enter the second value=> '))
+variable3 = variable1 % variable2
+print(variable3)
+
+# for power value
+print(2**2)
+
+# for roots value
+print(4**0.5)
