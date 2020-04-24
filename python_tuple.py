3dfd3bfb4bbd2ac67cd55cd52742b26d94739726
SwarupKarmokar <59216779+SwarupKarmokar@users.noreply.github.com>
2020-04-24 21:33:45 +0530

    methods added into tuple

--- python_tuple.py
+++ python_tuple.py
@@ -12,7 +12,13 @@ print(my_tuple[::-1])
 # we can find index position values
 print(my_tuple[3]['k1'])
 print(my_tuple[2][3])
+# try
+print(my_tuple[1].upper())
 
 # mutable checkup
 my_tuple[0] = 'String'
 print(my_tuple)  # its shows an error
+
+# tuple methods
+my_tuple.index('string')  # return index position
+my_tuple.count('string')  # counts the number of time a value appears
