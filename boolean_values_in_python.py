9285e96379759a9e19f68daee9a7b1d4809ccdcf
SwarupKarmokar <59216779+SwarupKarmokar@users.noreply.github.com>
2020-04-25 21:26:34 +0530

    booleans added

---
+++ boolean_values_in_python.py
@@ -0,0 +1,26 @@
+# note that smaller case t or f shows error
+a = True
+print(a)
+b = False
+print(b)
+
+# try (its return true or false)
+print(1 > 2)
+print(2 > 1)
+
+# using logical operator=>
+a = 1
+b = 2
+print(a > b and a < b) # using and operator.
+# its return False because True && False = False
+
+print(a > b or a < b) # using or operator
+# its return True value because True or False = True
+
+print(not a) # use of not operator
+# try
+a = True
+b = False
+print(not a) # return False
+print(not b) # return True
+
