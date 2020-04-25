421cdc05cc74893b2e42450881ae3a3ba44d0db6
SwarupKarmokar <59216779+SwarupKarmokar@users.noreply.github.com>
2020-04-25 16:39:41 +0530

    some pattern added

---
+++ pattern_programming.py
@@ -0,0 +1,17 @@
+# try
+for item in range(4):
+    for item in range(4):
+         print('*',end= ' ')
+    print()
+
+# try
+for item in range(4):
+    for item in range(item + 1):
+        print('*', end= ' ')
+    print()
+
+# try
+for item in range(4):
+    for item in range(4 - item):
+        print('*', end= ' ')
+    print()
