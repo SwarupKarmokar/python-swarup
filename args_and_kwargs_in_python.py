96a807dacad99edaab3582b88c6f8d013fc22efa
SwarupKarmokar <59216779+SwarupKarmokar@users.noreply.github.com>
2020-05-06 20:54:10 +0530

    mix example added

--- args_and_kwargs_in_python.py
+++ args_and_kwargs_in_python.py
@@ -13,3 +13,26 @@ def example_func1(*args): # it take whatever name we give after *: like: *hello
 ans2 = example_func1(1,2,3,4,5,6,7)
 print(ans2)
 # when a function argument start with 'asterisk(*)' its allows programmer to take an arbitary number of arguments and take them in tule values
+
+# try
+def string_example(*text):
+    print(' '.join(text))
+
+string_example('swarup', 'souvik', 'saiful', 'kishore')
+
+# *kwargs: its builds a dictionary of key value pairs.
+def my_func(**kwargs):
+    if 'keys' in kwargs:
+        print(f"my favarite fruit is {kwargs['keys']} ")
+
+my_func(keys= 'orange')
+my_func(keys= 'pianple')
+
+# try
+def my_func(*args, **kwargs):
+    if 'fruit' and 'juice' in kwargs:
+        print(f"i love to eat {' and '.join(args) } and my favrite food is {kwargs['fruit']}")
+        print(f"give me some {kwargs['juice']}")
+    else:
+        pass
+my_func('egg','chicken',fruit='banana',juice='orange juice')
